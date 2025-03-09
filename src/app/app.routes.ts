cd267968a2f2b765de761ae01297ef7d74723419
Dario Cabezas <dariosce99@gmail.com>
2025-03-09 08:27:00 -0500

    Delivery Maquetado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { DeliveryComponent } from './pages/delivery/delivery.component';
 
 export const routes: Routes = [
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-  {
-    path: 'home',
-    component: HomeComponent,
-  },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'delivery', component: DeliveryComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
