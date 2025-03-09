84a43eafe3f4745ff5e711eb36901705abed1195
Dario Cabezas <dariosce99@gmail.com>
2025-03-09 15:42:35 -0500

    Screen Crear Conrtapago y Mapa

--- src/app/pages/delivery _creacion/delivery-create.component.ts
+++ src/app/pages/delivery _creacion/delivery-create.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; 
+import { CommonModule } from '@angular/common';
 import { FooterNavComponent } from '../shared/footer-nav/footer-nav.component';
 
 @Component({
@@ -8,7 +8,12 @@ import { FooterNavComponent } from '../shared/footer-nav/footer-nav.component';
   imports: [
     CommonModule,
     FooterNavComponent,
-],
+  ],
   templateUrl: './delivery-create.component.html',
 })
-export class DeliveryCreateComponent { }
+export class DeliveryCreateComponent {
+  abrirNuevaPagina() {
+    const url = "https://deuna.app/mapa?ubicacion=-0.208903,-78.501191,19z";
+    window.open(url, '_self');
+  }
+}
