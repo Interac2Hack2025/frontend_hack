f0aeb9d0a5a32d54251439aa95f8cde9882cebd7
Dario Cabezas <dariosce99@gmail.com>
2025-03-09 07:05:52 -0500

    Colores

--- src/app/pages/home/components/action-grid/action-grid.component.ts
+++ src/app/pages/home/components/action-grid/action-grid.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 interface Action {
   name: string;
   icon: string;
+  color: string;
 }
 
 @Component({
@@ -18,14 +19,14 @@ export class ActionGridComponent {
   }
   
   actions: Action[] = [
-    { name: 'Transferir', icon: 'payments' },
-    { name: 'Recargar', icon: 'credit_card' },
-    { name: 'Cobrar', icon: 'calculate' },
-    { name: 'Metro de Quito', icon: 'train' },
-    { name: 'Invita y Gana', icon: 'celebration' },
-    { name: 'Deuna Jóvenes', icon: 'group' },
-    { name: 'Verificar pago', icon: 'verified' },
-    { name: 'Contraentrega', icon: 'local_shipping' }
+    { name: 'Transferir', icon: 'payments', color: '#4A90E2' }, 
+    { name: 'Recargar', icon: 'credit_card', color: '#50C878' }, 
+    { name: 'Cobrar', icon: 'calculate', color: '#FF5733' }, 
+    { name: 'Metro de Quito', icon: 'train', color: '#FFD700' }, 
+    { name: 'Invita y Gana', icon: 'celebration', color: '#8A2BE2' }, 
+    { name: 'Deuna Jóvenes', icon: 'group', color: '#FF69B4' }, 
+    { name: 'Verificar pago', icon: 'verified', color: '#32CD32' }, 
+    { name: 'Contraentrega', icon: 'local_shipping', color: '#FF8C00' }  
   ];
   
 }
